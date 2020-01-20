ab8f0266534a08cc9cd256f32d328423965fe9c8
Sara Harvey <saraharveynyc@gmail.com>
2020-01-20 17:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 
 function writeCards(names, event_name) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful surprise ${event_name} gift!`);
+  for (let i = 0; i < names.length; i++){
+    names[i] = (`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
   }
   return names;
 }

a026eb6d7e0a3e663a691372bac9adfbaa9eba8b
Sara Harvey <saraharveynyc@gmail.com>
2020-01-20 17:35:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 
 function writeCards(names, event_name) {
   for (let i = 0; i < names.length; i++){
-    names[i] = (`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
+    (`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
   }
-  return names;
-}

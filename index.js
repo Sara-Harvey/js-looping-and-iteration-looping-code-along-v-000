f3d3ab38c8a1f3802050cd85bec4a4d81542d47e
Sara Harvey <saraharveynyc@gmail.com>
2020-01-20 17:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Code your solutions in this file
 
-function writeCards(names, eventname)
+function writeCards(names, event_name) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful surprise ${event_name[i]} gift!`);
+  }
+  return names;
+}
  
 function wrapGifts(gifts) {
   for (let i = 0; i < gifts.length; i++) {
@@ -9,3 +14,7 @@ function wrapGifts(gifts) {
  
   return gifts;
 }
+
+    it( 'returns an array of thank you messages for each name provided to the function', () => {
+      expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
+        .to.deep.eq( [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ] );
